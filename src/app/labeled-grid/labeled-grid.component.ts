f6075f21c92a1318281d319077b1bec7d99f533c
任浩杰 <renhaojie520@live.cn>
2019-07-03 11:30:35 +0800

    labeled-grid

--- src/app/labeled-grid/labeled-grid.component.ts
+++ src/app/labeled-grid/labeled-grid.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import * as THREE from 'three';
+import LabeledGrid from './labeled-grid';
+import TrackballControls from '../controls/TrackballControls';
 @Component({
   selector: 'app-labeled-grid',
   templateUrl: './labeled-grid.component.html',
@@ -15,7 +17,28 @@ export class LabeledGridComponent implements OnInit {
 
   ngOnInit() {
     this.initThree();
+    this.addLight();
+    this.addGrid();
     this.update();
+  }
+  addGrid() {
+    const grid = new LabeledGrid(
+      100,
+      100,
+      10,
+      [0, 1, 0],
+      0x000055,
+      0.2,
+      true,
+      '#000000',
+      'left'
+    );
+    this.scene.add(grid);
+  }
+  addLight() {
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(100, 80, 130);
+    this.scene.add(light);
   }
   initThree() {
     this.scene = new THREE.Scene();
@@ -27,9 +50,17 @@ export class LabeledGridComponent implements OnInit {
     this.el.nativeElement.appendChild(this.renderer.domElement);
 
     this.camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+    this.camera.position.set(90, 90, 90);
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    this.controls = new TrackballControls(
+      this.camera,
+      this.renderer.domElement
+    );
   }
   update() {
     this.renderer.render(this.scene, this.camera);
+    this.controls.update();
 
     requestAnimationFrame(this.update.bind(this));
   }
--- src/app/labeled-grid/labeled-grid.ts
+++ src/app/labeled-grid/labeled-grid.ts
@@ -13,6 +13,9 @@ export default class LabeledGrid extends THREE.Object3D {
   stepSubDivisions: number;
   mainGrid: THREE.LineSegments;
   subGrid: THREE.LineSegments;
+  margin: THREE.LineSegments;
+  labelStore: {};
+  labels: any;
   constructor(
     width = 200,
     length = 200,
@@ -79,7 +82,7 @@ export default class LabeledGrid extends THREE.Object3D {
 
     for (
       let i = 0;
-      i < this.width / 2;
+      i <= this.width / 2;
       i += this.step / this.stepSubDivisions
     ) {
       subGridGeometry.vertices.push(
@@ -98,19 +101,25 @@ export default class LabeledGrid extends THREE.Object3D {
 
       if (i % this.step === 0) {
         gridGeometry.vertices.push(
-          new THREE.Vector3(-length / 2, i, mainGridZ)
-        );
-        gridGeometry.vertices.push(new THREE.Vector3(length / 2, i, mainGridZ));
-        gridGeometry.vertices.push(
-          new THREE.Vector3(-length / 2, -i, mainGridZ)
+          new THREE.Vector3(-this.length / 2, i, mainGridZ)
         );
         gridGeometry.vertices.push(
-          new THREE.Vector3(length / 2, -i, mainGridZ)
+          new THREE.Vector3(this.length / 2, i, mainGridZ)
+        );
+        gridGeometry.vertices.push(
+          new THREE.Vector3(-this.length / 2, -i, mainGridZ)
+        );
+        gridGeometry.vertices.push(
+          new THREE.Vector3(this.length / 2, -i, mainGridZ)
         );
       }
     }
 
-    for (let i = 0; i <= length / 2; i += this.step / this.stepSubDivisions) {
+    for (
+      let i = 0;
+      i <= this.length / 2;
+      i += this.step / this.stepSubDivisions
+    ) {
       subGridGeometry.vertices.push(
         new THREE.Vector3(i, -this.width / 2, subGridZ)
       );
@@ -154,28 +163,28 @@ export default class LabeledGrid extends THREE.Object3D {
       new THREE.Vector3(-this.length / 2, -this.width / 2, subGridZ)
     );
     marginGeometry.vertices.push(
-      new THREE.Vector3(length / 2, -this.width / 2, subGridZ)
+      new THREE.Vector3(this.length / 2, -this.width / 2, subGridZ)
     );
 
     marginGeometry.vertices.push(
-      new THREE.Vector3(length / 2, -this.width / 2, subGridZ)
+      new THREE.Vector3(this.length / 2, -this.width / 2, subGridZ)
     );
     marginGeometry.vertices.push(
-      new THREE.Vector3(length / 2, this.width / 2, subGridZ)
-    );
-
-    marginGeometry.vertices.push(
-      new THREE.Vector3(length / 2, this.width / 2, subGridZ)
-    );
-    marginGeometry.vertices.push(
-      new THREE.Vector3(-length / 2, this.width / 2, subGridZ)
+      new THREE.Vector3(this.length / 2, this.width / 2, subGridZ)
     );
 
     marginGeometry.vertices.push(
-      new THREE.Vector3(-length / 2, this.width / 2, subGridZ)
+      new THREE.Vector3(this.length / 2, this.width / 2, subGridZ)
     );
     marginGeometry.vertices.push(
-      new THREE.Vector3(-length / 2, -this.width / 2, subGridZ)
+      new THREE.Vector3(-this.length / 2, this.width / 2, subGridZ)
+    );
+
+    marginGeometry.vertices.push(
+      new THREE.Vector3(-this.length / 2, this.width / 2, subGridZ)
+    );
+    marginGeometry.vertices.push(
+      new THREE.Vector3(-this.length / 2, -this.width / 2, subGridZ)
     );
 
     marginGeometry.vertices.push(
@@ -205,5 +214,111 @@ export default class LabeledGrid extends THREE.Object3D {
     marginGeometry.vertices.push(
       new THREE.Vector3(-offsetLength / 2, -offsetWidth / 2, subGridZ)
     );
+
+    const strongGridMaterial = new THREE.LineBasicMaterial({
+      color: new THREE.Color().setHex(this.color),
+      opacity: this.opacity * 2,
+      linewidth: 2,
+      transparent: true
+    });
+
+    this.margin = new THREE.LineSegments(marginGeometry, strongGridMaterial);
+
+    this.add(this.mainGrid);
+    this.add(this.subGrid);
+    this.add(this.margin);
+
+    this.drawNumbering();
+  }
+  private drawNumbering() {
+    this.labelStore = {};
+    if (this.labels != null) {
+      this.mainGrid.remove(this.labels);
+    }
+
+    this.labels = new THREE.Object3D();
+
+    const labelsFront = new THREE.Object3D();
+    const labelsSideRight = new THREE.Object3D();
+
+    for (let i = 0; i <= this.width / 2; i += this.step) {
+      const sizeLabel = this.drawTextOnPlane('' + i);
+      const sizeLabel2 = sizeLabel.clone();
+
+      sizeLabel.position.set(this.length / 2, -i, 0.1);
+      sizeLabel.rotation.z = -Math.PI / 2;
+      labelsFront.add(sizeLabel);
+
+      sizeLabel2.position.set(this.length / 2, i, 0.1);
+      sizeLabel2.rotation.z = -Math.PI / 2;
+      labelsFront.add(sizeLabel2);
+    }
+
+    for (let i = 0; i <= this.length / 2; i += this.step) {
+      const sizeLabel = this.drawTextOnPlane('' + i);
+      const sizeLabel2 = sizeLabel.clone();
+
+      sizeLabel.position.set(-i, this.width / 2, 0.1);
+      labelsSideRight.add(sizeLabel);
+
+      sizeLabel2.position.set(i, this.width / 2, 0.1);
+      labelsSideRight.add(sizeLabel2);
+    }
+
+    const labelsSideLeft = labelsSideRight.clone();
+    labelsSideLeft.rotation.z = -Math.PI;
+
+    const labelsBack = labelsFront.clone();
+    labelsBack.rotation.z = -Math.PI;
+
+    this.labels.add(labelsFront);
+    this.labels.add(labelsBack);
+    this.labels.add(labelsSideLeft);
+    this.labels.add(labelsSideRight);
+
+    this.labels.traverse(child => {
+      child.visible = this.text;
+    });
+
+    this.mainGrid.add(this.labels);
+  }
+  drawTextOnPlane(text) {
+    let canvas, context, material, plane, texture;
+
+    const size = 128;
+    canvas = document.createElement('canvas');
+    canvas.width = 128;
+    canvas.height = 128;
+
+    context = canvas.getContext('2d');
+    context.font = '18px sans-serif';
+    context.textAlign = 'center';
+    context.fillStyle = this.textColor;
+    context.fillText(text, canvas.width / 2, canvas.height / 2);
+    context.strokeStyle = this.textColor;
+    context.strokeText(text, canvas.width / 2, canvas.height / 2);
+
+    texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+    texture.generateMipmaps = true;
+    texture.magFilter = THREE.LinearFilter;
+    texture.minFilter = THREE.LinearFilter;
+
+    material = new THREE.MeshBasicMaterial({
+      map: texture,
+      transparent: true,
+      color: 0xffffff,
+      alphaTest: 0.3
+    });
+
+    plane = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(size / 8, size / 8),
+      material
+    );
+
+    plane.doubleSided = true;
+    plane.overdraw = true;
+
+    return plane;
   }
 }
