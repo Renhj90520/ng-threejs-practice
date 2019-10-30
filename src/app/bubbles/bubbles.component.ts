3ab58c44177a03240f454e78fbc5820ca8494b95
RenHaojie <renhaojie520@live.cn>
2019-10-31 07:46:40 +0800

    save

--- src/app/bubbles/bubbles.component.ts
+++ src/app/bubbles/bubbles.component.ts
@@ -42,12 +42,38 @@ export class BubblesComponent implements OnInit {
 
     this.group.add(this.mainSphere);
     this.scene.add(this.group);
+
+    for (let i = 0; i < this.mainSphere.geometry.faces.length; i++) {
+      const face: any = this.mainSphere.geometry.faces[i];
+      const geometry: any = this.mainSphere.geometry;
+
+      const newSphere: any = new THREE.Mesh(
+        new THREE.IcosahedronGeometry(20, 1),
+        secondMat
+      );
+      face.centroid = new THREE.Vector3(0, 0, 0);
+      face.centroid.add(geometry.vertices[face.a]);
+      face.centroid.add(geometry.vertices[face.b]);
+      face.centroid.add(geometry.vertices[face.c]);
+      face.centroid.divideScalar(3);
+      newSphere.position.set(face.centroid.x, face.centroid.y, face.centroid.z);
+      newSphere.target = new THREE.Vector3(
+        Math.random() * 200 - 100,
+        Math.random() * 200 - 100,
+        Math.random() * 200 - 100
+      );
+      newSphere.base = new THREE.Vector3(
+        newSphere.position.x,
+        newSphere.position.y,
+        newSphere.position.z
+      );
+
+      this.group.add(newSphere);
+    }
   }
   addLights() {
     const mainLight: any = new THREE.PointLight(0xd29553);
     mainLight.position.set(50, 200, 400);
-    console.log(mainLight.target);
-    mainLight.target = this.mainSphere;
     this.scene.add(mainLight);
     const ambientLight = new THREE.AmbientLight(0x2a4159);
     this.scene.add(ambientLight);
@@ -64,7 +90,41 @@ export class BubblesComponent implements OnInit {
     this.renderer.setSize(width, height);
     this.el.nativeElement.appendChild(this.renderer.domElement);
   }
+
+  t = 0;
+  decell = false;
+  lastP = 0;
   update() {
+    this.t++;
+    const progress = Math.sin(this.t / 13) / 2 + 0.5;
+    this.group.children[0].scale.x = 1 + progress / 5;
+    this.group.children[0].scale.y = 1 + progress / 5;
+    this.group.children[0].scale.z = 1 + progress / 5;
+
+    for (let i = 1; i < this.group.children.length; i++) {
+      const sphere: any = this.group.children[i];
+      sphere.position.x = sphere.base.x + sphere.target.x * progress;
+      sphere.position.y = sphere.base.y + sphere.target.y * progress;
+      sphere.position.z = sphere.base.z + sphere.target.z * progress;
+    }
+
+    if (progress < this.lastP && !this.decell) {
+      this.decell = true;
+    }
+    if (progress > this.lastP && this.decell) {
+      for (let i = 1; i < this.group.children.length; i++) {
+        const sphere: any = this.group.children[i];
+        sphere.target = new THREE.Vector3(
+          Math.random() * 200 - 100,
+          Math.random() * 200 - 100,
+          Math.random() * 200 - 100
+        );
+      }
+      this.decell = false;
+    }
+    this.lastP = progress;
+
+    this.group.rotation.y += 1.5 * (Math.PI / 180);
     this.renderer.render(this.scene, this.camera);
 
     requestAnimationFrame(this.update.bind(this));
